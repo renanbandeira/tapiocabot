c16d279b2696522efc146d69a10a373e638c62b8
Renan Bandeira <email@renanbandeira.com.br>
2019-12-19 16:00:14 -0300

    Inits project

---
+++ src/functions.js
@@ -0,0 +1,60 @@
+function formatDate(date) {
+  return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+}
+
+function getOrdersFromDay(msg, ordersRef, bot, date) {
+  ordersRef.child(date).once('value').then(function(snapshot) {
+    const orders = snapshot.val() || {};
+    console.log('orders', orders);
+    let ordersString = "Pedidos (" + date + ")";
+    if (Object.keys(orders).length === 0) {
+      ordersString += "\n Sem pedidos para essa data";
+    }
+    Object.keys(orders).forEach(orderKey => {
+      ordersString += "\n" + orderKey + " - " + orders[orderKey];
+    });
+    bot.sendMessage(msg.chat.id, ordersString);
+  });
+}
+
+module.exports = {
+  listOrders: function(msg, ordersRef, bot) {
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+    let date = formatDate(today);
+    if (today.getHours() >= 8) {
+      date = formatDate(tomorrow);
+    }
+    getOrdersFromDay(msg, ordersRef, bot, date);
+  },
+  addOrder: function(msg, orderQty, ordersRef, bot) {
+    let qty = Number(orderQty);
+    const userName = msg.from.first_name;
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+    let date = formatDate(today);
+    let msgToAnswer = "Então, vou dar um jeitinho pra reservar pra ti ainda hoje (";
+    if (today.getHours() >= 8) {
+      date = formatDate(tomorrow);
+      msgToAnswer = "Blz ma, vou registrar pra amanhã (";
+    }
+    if (qty <= 0) {
+      qty = null;
+      bot.sendMessage(msg.chat.id, "Tranquilo, man. Vou cancelar teu pedido");
+    } else if (qty > 30){
+      bot.sendMessage(msg.chat.id, "Vai, guloso..a produção não dá pra fazer tanto assim não =/ Máx: 30");
+    } else {
+      bot.sendMessage(msg.chat.id, msgToAnswer + date + ")");
+    }
+    ordersRef.child(date).child(userName).set(qty, function(error) {
+      if (!error) {
+        getOrdersFromDay(msg, ordersRef, bot, date);
+      } else {
+        console.log('error 2', error);
+      }
+    });
+    // getOrdersFromDay(msg, ordersRef, bot, date);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,111 @@
+const TelegramBot = require('node-telegram-bot-api');
+const firebase = require('firebase');
+
+const token = require('./security/token');
+const bot = new TelegramBot(token, {polling: true});
+const operations = require('./functions');
+
+const firebaseConfig = require('./security/firebaseConfig');
+const auth = require('./security/auth');
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function login() {
+  return firebase.auth().signInWithEmailAndPassword(auth.email, auth.password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.error("Error while singing in: " + error.code);
+    console.error(error.message);
+  });
+}
+
+const ref = firebase.database().ref();
+const ordersRef = ref.child("orders");
+
+/*
+function listOrders(msg) {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  let date = formatDate(today);
+  if (today.getHours() >= 8) {
+    date = formatDate(tomorrow);
+  }
+
+  ordersRef.child(date).once('value').then(function(snapshot) {
+    const orders = snapshot.val() || [];
+    console.log(orders);
+    let ordersString = "Pedidos (" + date + ")";
+    orders.forEach(order => {
+      ordersString += "\n" + order.name + " - " + order.qty;
+    });
+    bot.sendMessage(msg.chat.id, ordersString);
+  });
+}
+
+
+function addOrder(msg, orderQty) {
+  const userName = msg.from.first_name;
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  let date = formatDate(today);
+  if (today.getHours() < 8) {
+    bot.sendMessage(msg.chat.id, "Então, vou dar um jeitinho pra reservar pra ti ainda hoje (" +
+      date +")");
+  } else {
+    date = formatDate(tomorrow);
+    bot.sendMessage(msg.chat.id, "Blz ma, vou registrar pra amanhã (" +
+      date +")");
+  }
+
+  ordersRef.child(date).push().set({
+    name: userName,
+    qty: orderQty
+  });
+
+  ordersRef.child(date).once('value').then(function(snapshot) {
+    const orders = snapshot.val() || [];
+    console.log(orders);
+    let ordersString = "Pedidos (" + date + ")";
+    orders.forEach(order => {
+      ordersString += "\n" + order.name + " - " + order.qty;
+    });
+    bot.sendMessage(msg.chat.id, ordersString);
+  });
+}
+
+*/
+
+bot.onText(/\/pedidos/, (msg, match) => {
+  login();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('user is signed in');
+      operations.listOrders(msg, ordersRef, bot);
+    } else {
+      console.log('user is signed out');
+    }
+  });
+});
+
+bot.onText(/\/quero (\D+)/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, "Isso é um NaN, man! Só aceito dígito pra pedir tapioca", { reply_to_message_id: msg.message_id });
+});
+
+bot.onText(/\/cobrar/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Ei galera, esquece de pagar não =) " + "https://picpay.me/renanbandeira");
+});
+
+bot.onText(/\/quero (\d+)/, (msg, match) => {
+  login();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('user is signed in');
+      operations.addOrder(msg, match[1], ordersRef, bot);
+    } else {
+      console.log('user is signed out');
+    }
+  });
+});
