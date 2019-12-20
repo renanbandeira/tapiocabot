65764fe7634a53d174fd494c5a1799e40c368d1a
Renan Bandeira <email@renanbandeira.com.br>
2019-12-20 13:29:38 -0300

    Adds security tokens env

---
+++ src/security/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_URL,
+  projectId: process.env.FIREBASE_PROJECT,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDERID,
+  appId: process.env.FIREBASE_APPID
+};
